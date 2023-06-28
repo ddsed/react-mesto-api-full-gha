bc7f83b28d10ac7331f9db90e98b1e0645dbfc0b
Dasha Sedova <dashassed@gmail.com>
2023-06-28 09:50:27 +0300

    fix: minor alterations

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-require('dotenv').config();
+// require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
