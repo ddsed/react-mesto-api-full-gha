a4121a88f537751d414bec57f66ebaa6a7c13ac1
Dasha Sedova <dashassed@gmail.com>
2023-07-02 13:42:32 +0300

    fix: edituser

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -80,7 +80,8 @@ const createUser = (req, res, next) => {
 };
 
 const updateUser = (req, res, next) => {
-  userModel.findByIdAndUpdate(req.user._id, req.body, { new: true, runValidators: true })
+  const { name, about } = req.body;
+  userModel.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
     .orFail(() => {
       throw new NotFoundError('Пользователь не найден');
     })
